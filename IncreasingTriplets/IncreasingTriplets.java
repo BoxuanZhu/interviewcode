ee5a88e32fceba32bebb96d6376e15adbd217cd9
Boxuan Zhu <BoxuanZhu@Boxuans-MacBook-Pro.local>
2017-05-29 10:24:55 +1000

    use 2 var to track the 2 smallest number throughout

---
+++ IncreasingTriplets/IncreasingTriplets.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int a = Integer.MAX_VALUE;
+        int b = Integer.MAX_VALUE;
+        for(int x : nums){
+            if(x <= a)
+                a = x;
+            else if(x <= b)
+                b = x;
+            else
+                return true;
+        }
+        return false;
+    }
+}
