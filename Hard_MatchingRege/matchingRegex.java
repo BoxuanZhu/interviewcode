e9aecce66917605fa7e35ee83512402d9cd9fd5f
Boxuan Zhu <BoxuanZhu@Boxuans-MacBook-Pro.local>
2017-06-01 10:39:53 +1000

    matching regex done in dp, java

---
+++ Hard_MatchingRege/matchingRegex.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public boolean isMatch(String s, String p) {
+        int slen = s.length();
+        int plen = p.length();
+        boolean[][] dp = new boolean[slen+1][plen+1];
+        dp[0][0] = true;
+        for(int i = 1; i < plen+1; i++){
+            if(p.charAt(i-1) == '*')
+                dp[0][i] = dp[0][i-2];
+        }
+        
+        for(int i = 1; i < slen + 1; i ++){
+            for(int j = 1; j < plen+1;j++){
+                if(p.charAt(j-1) == '.'||p.charAt(j-1) == s.charAt(i-1))
+                    dp[i][j] =dp[i-1][j-1];
+                else if(p.charAt(j-1) == '*'){
+                    dp[i][j] = dp[i][j-2];
+                    if(p.charAt(j-2) == '.'||p.charAt(j-2) == s.charAt(i-1)){
+                        dp[i][j] =dp[i-1][j]|dp[i][j];
+                        
+                    }
+                        
+                }
+                else
+                    dp[i][j] = false;
+            }
+        }
+        return dp[slen][plen];
+    }
+}
